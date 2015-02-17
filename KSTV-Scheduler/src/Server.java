e354354a756b969b383de581b9c4a6b7ac37c235
Shigarevsky Dmitry <radix.ru@gmail.com>
2015-02-18 00:35:49 +0300

    KSTV-Scheduler: Use HTTP status codes insted of return parameter, use different HTTP methods

--- KSTV-Scheduler/src/Server.java
+++ KSTV-Scheduler/src/Server.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class Server extends HttpServlet {
 	@Override
@@ -25,8 +26,10 @@ public class Server extends HttpServlet {
 
 	@Override
 	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		BufferedReader reader = req.getReader();
+		HttpSession session = req.getSession();
+		boolean sessionIsNew = session.isNew();
 
+		BufferedReader reader = req.getReader();
 		String r = reader.readLine(); // request should contain exactly one line
 
 		HashMap<String, String> pars = new HashMap<String, String>();
@@ -39,6 +42,46 @@ public class Server extends HttpServlet {
 		res.setContentType("text/plain");
 
 		PrintWriter pw = res.getWriter();
+
+		String action = pars.get("action");
+		if (action == null) {
+			pw.print("result=lexical_error");
+		} else switch (action) {
+		case "start_session":
+			String login = pars.get("login");
+			String password = pars.get("password");
+			String listen_port = pars.get("listen_port");
+			if (login == null || password == null || listen_port == null) {
+				pw.print("result=lexical_error");
+			} else {
+				;
+			}
+			break;
+		case "create_user":
+			;
+			break;
+		case "change_user":
+			;
+			break;
+		case "user_info":
+			;
+			break;
+		case "add_event":
+			;
+			break;
+		case "remove_event":
+			;
+			break;
+		case "add_random_event":
+			;
+			break;
+		case "clone_event":
+			;
+			break;
+		default:
+			pw.print("result=lexical_error");
+		}
+
 		pw.close();
 	}
 }
