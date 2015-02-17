75b079e113420a16434e7c7915f46ba9244cc64a
Shigarevsky Dmitry <radix.ru@gmail.com>
2015-02-17 17:04:21 +0300

    KSTV-Scheduler: Overview, protocol changes, initial doPost

--- KSTV-Scheduler/src/Server.java
+++ KSTV-Scheduler/src/Server.java
@@ -1,5 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -7,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class Server extends HttpServlet {
+	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		res.setContentType("text/html");
 
@@ -17,6 +20,25 @@ public class Server extends HttpServlet {
 		pw.println(" </body>");
 		pw.println("</html>");
 
+		pw.close();
+	}
+
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		BufferedReader reader = req.getReader();
+
+		String r = reader.readLine(); // request should contain exactly one line
+
+		HashMap<String, String> pars = new HashMap<String, String>();
+		String[] kvs = r.split("&");
+		for (String kv: kvs) {
+			int i = kv.indexOf('=');
+			pars.put(kv.substring(0, i), kv.substring(i + 1));
+		}
+
+		res.setContentType("text/plain");
+
+		PrintWriter pw = res.getWriter();
 		pw.close();
 	}
 }
