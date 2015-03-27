4c75f7cceb5d1051ecd05fd4069cf233dfad838c
Shigarevsky Dmitry <radix.ru@gmail.com>
2015-03-27 22:34:17 +0300

    KSTV-Scheduler: Shared and SyntaxChecker Shared: 	1. Remove deprecated methods: 		1. createPartsScanner 		2. Old versions of getParts() 		3. checkMsgDelimiterNotFound 		4. checkTextPastTheLastPart 	2. Convert delimiter constants from String to char and make them public 	3. getParts: Use empty string instead of null as ignoredPart if there isn't one 	4. putParts: Drop Objects.requireNonNull 	5. GetPartsResult: Swap endOfStream and ignoredPart 	6. Remove comments about streams and readers 	7. Java docs formatting SyntaxChecker: 	1. Replace manual checking with pattern matching 	2. Rename checkTimeZone -> checkAbsTimeZone 	3. Add checkLocalDateTime 	4. Add Java docs

--- KSTV-Scheduler/Shared.java
+++ KSTV-Scheduler/Shared.java
@@ -1,133 +1,58 @@
 import java.util.*;
 import java.io.*;
 
-/**
- * Provides methods shared between Server and Client.
- */
+/** Provides methods shared between Server and Client. */
 public final class Shared {
 	/**
 	 * Message part delimiter.
 	 * <p> U+001F INFORMATION SEPARATOR ONE, a.k.a. unit separator (US).
 	 */
-	private static final String msgPartDelimiter = "\u001F";
+	public static final char msgPartDelimiter = '\u001F';
+
+	private static final String msgPartDelimiterStr = String.valueOf(msgPartDelimiter);
 
 	/**
 	 * Message delimiter.
 	 * <p> U+001E INFORMATION SEPARATOR TWO, a.k.a. record separator (RS).
 	 */
-	private static final String msgDelimiter = "\u001E";
+	public static final char msgDelimiter = '\u001E';
 
-	/**
-	 * Constructs and prepares a scanner to be used in {@link #getParts(Scanner)}.
-	 * @param	reader A reader to read from.
-	 * @return	Constructed and prepared scanner.
-	 */
-	@Deprecated
-	public static Scanner createPartsScanner(Reader reader) {
-		Scanner scanner = new Scanner(reader);
-		scanner.useDelimiter(msgDelimiter);
-		return scanner;
-	}
-
-	/**
-	 * Reads a message from a scanner and splits it into parts.
-	 * <p> Skips the scanner past the first message delimiter found,
-	 * than splits the read string into message parts.
-	 * <p> NB: The scanner should have been obtained via {@link #createPartsScanner(Reader)}.
-	 * <p> NB: The result should be checked against {@link #checkMsgDelimiterNotFound(String[])} and
-	 * {@link #checkTextPastTheLastPart(String[])} and handled appropriately.
-	 * <p> NB: If either error is present the result is unspecified.
-	 * @param	scanner A scanner to scan from.
-	 * @return	Array of message parts, or {@link #endOfStream} or
-	 * {@link #textPastTheLastPart} if appropriate.
-	 */
-	@Deprecated
-	public static String[] getParts(Scanner scanner) {
-		Objects.requireNonNull(scanner);
-
-		String message = "";
-
-		boolean msgDelimiterNotFound = false;
-		boolean textPastTheLastPart = false;
-
-		// scanner.next() would skip the delimiter if it was the first character
-		// so we must check manually
-		try {
-			scanner.skip(msgDelimiter);
-			return new String[] {};
-		} catch (NoSuchElementException e) {
-			try {
-				message = scanner.next();
-			} catch (NoSuchElementException e2) {
-				msgDelimiterNotFound = true;
-			}
-		}
-
-		String[] parts = message.split(msgPartDelimiter, -1);
-		if (!parts[parts.length - 1].isEmpty()) {
-			textPastTheLastPart = true;
-		}
-
-		if (msgDelimiterNotFound || textPastTheLastPart) {
-			String[] result = Arrays.copyOf(parts, parts.length + 1);
-			result[parts.length] =
-				(msgDelimiterNotFound ? msgDelimiter : "") +
-				(textPastTheLastPart ? msgPartDelimiter : "");
-			return result;
-		}
-		return Arrays.copyOfRange(parts, 0, parts.length - 1);
-	}
-
-	/**
-	 * Contains result of {@link #getParts2}.
-	 */
+	/** Contains result of {@link #getParts}. */
 	public static class GetPartsResult {
-		/**
-		 * Array of parts. Ignored part is not part of this array.
-		 */
+		/** Array of parts. Ignored part is not included in this array. */
 		public final String[] parts;
-		/**
-		 * Whether end of stream was encountered before first message delimiter.
-		 */
-		public final boolean endOfStream;
-		/**
-		 * Ignored part. Empty if there isn't one.
-		 */
+
+		/** Ignored part. Empty if there isn't one. */
 		public final String ignoredPart;
-		/**
-		 * Any exception thrown by internal IO operations. null if there isn't one.
-		 */
+
+		/** Whether end of stream was encountered before first message delimiter. */
+		public final boolean endOfStream;
+
+		/** Any exception thrown by internal IO operations. null if there isn't one. */
 		public final IOException ioe;
 
-		public GetPartsResult(String[] parts, boolean endOfStream, String ignoredPart, IOException ioe) {
+		public GetPartsResult(String[] parts, String ignoredPart, boolean endOfStream, IOException ioe) {
 			this.parts = parts;
-			this.endOfStream = endOfStream;
 			this.ignoredPart = ignoredPart;
+			this.endOfStream = endOfStream;
 			this.ioe = ioe;
 		}
 	}
 
-	// ServletRequest.getInputStream() -> ServletInputStream
-	// ServletRequest.getReader() -> BufferedReader
-	// Socket.getInputStream() -> InputStream
-	// HttpURLConnection.getErrorStream() -> InputStream
-	// HttpURLConnection.getInputStream() -> InputStream
 	/**
 	 * Reads a message from a reader and splits it into parts.
-	 * <p> Skips the reader past the first message delimiter found,
-	 * than splits the read string into message parts.
+	 * <p> Skips the reader past the first message delimiter found, than splits the read string into message parts.
 	 * @param	reader A reader to read from.
-	 * @return	A {@link #GetPartsResult} containing parts, error info
-	 * and possibly thrown exception.
+	 * @return	A {@link #GetPartsResult} containing parts, error info and possibly thrown exception.
 	 */
-	public static GetPartsResult getParts2(Reader reader) {
+	public static GetPartsResult getParts(Reader reader) {
 		StringBuilder sb = new StringBuilder();
 
 		IOException ioe = null;
 		boolean endOfStream = false;
 		try {
 			int n = reader.read();
-			while (n != -1 && (char)n != '\u001E') {
+			while (n != -1 && (char)n != msgDelimiter) {
 				sb.append((char)n);
 				n = reader.read();
 			}
@@ -138,58 +63,11 @@ public final class Shared {
 			ioe = e;
 		}
 
-		String ignoredPart = null;
-		String[] parts = sb.toString().split(msgPartDelimiter, -1);
-		if (!parts[parts.length - 1].isEmpty()) {
-			ignoredPart = parts[parts.length - 1];
-		}
+		String[] parts = sb.toString().split(msgPartDelimiterStr, -1);
+		String ignoredPart = parts[parts.length - 1];
 		parts = Arrays.copyOfRange(parts, 0, parts.length - 1);
 
-		return new GetPartsResult(parts, endOfStream, ignoredPart, ioe);
-	}
-
-	/**
-	 * Special value indicating that the message delimiter was not found.
-	 * <p> Note: This reference should be unique.
-	 * @see		{@link #getParts(Scanner)}
-	 */
-	/**
-	 * Checks result of {@link #getParts(Scanner)} for "Message delimiter not found" error.
-	 * @param	parts Result to check.
-	 * @return	Whether the error was detected.
-	 */
-	@Deprecated
-	public static boolean checkMsgDelimiterNotFound(String[] parts) {
-		return parts.length > 0 && parts[parts.length - 1].startsWith(msgDelimiter);
-	}
-
-	/**
-	 * Special value indicating that there was some text after the last message part.
-	 * <p> Note: This reference should be unique.
-	 * @see		{@link #getParts(Scanner)}
-	 */
-	/**
-	 * Checks result of {@link #getParts(Scanner)} for "Text after last part" error.
-	 * @param	parts Result to check.
-	 * @return	Whether the error was detected.
-	 */
-	@Deprecated
-	public static boolean checkTextPastTheLastPart(String[] parts) {
-		return parts.length > 0 && parts[parts.length - 1].endsWith(msgPartDelimiter);
-	}
-
-	/**
-	 * Shortcut of {@link #getParts(Scanner)} for Reader.
-	 * <p> NB: This version closes the reader after using it.
-	 * @param	reader A reader to read from.
-	 * @return	Array of message parts, or {@link #endOfStream} or
-	 * {@link #textPastTheLastPart} if appropriate.
-	 */
-	@Deprecated
-	public static String[] getParts(Reader reader) {
-		try (Scanner msgSc = createPartsScanner(reader)) {
-			return getParts(msgSc);
-		}
+		return new GetPartsResult(parts, ignoredPart, endOfStream, ioe);
 	}
 
 	/**
@@ -199,9 +77,6 @@ public final class Shared {
 	 * @throws	IOException If any writing operation fails.
 	 */
 	public static void putParts(String[] parts, Writer writer) throws IOException {
-		Objects.requireNonNull(parts);
-		Objects.requireNonNull(writer);
-
 		for (String part: parts) {
 			writer.write(part);
 			writer.write(msgPartDelimiter);
--- KSTV-Scheduler/SyntaxChecker.java
+++ KSTV-Scheduler/SyntaxChecker.java
@@ -1,32 +1,54 @@
+import java.util.regex.*;
+
+/**
+ * Utilities for checking strings as valid representations of data types.
+ * <p> Data types and their valid representaions are described in <tt>server-interface-data-types.txt</tt>.
+ */
 public final class SyntaxChecker {
-	public static boolean isLetter(char c) {
-		return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || c == '_';
+	private static final Pattern NAME_PATTERN = Pattern.compile("[a-zA-Z_]\\w*");
+
+	/**
+	 * Checks whether a string is a valid user name.
+	 * <p> Checking is performed according to the UserName data type.
+	 * @param	name The string to check.
+	 * @return	<tt>true</tt> if <tt>name</tt> is valid, <tt>false</tt> otherwise.
+	 */
+	public static boolean checkName(String name) {
+		return NAME_PATTERN.matcher(name).matches();
 	}
 
-	public static boolean isDigit(char c) {
-		return '0' <= c && c <= '9';
+	private static final Pattern ABS_TIME_ZONE_PATTERN = Pattern.compile("GMT[-+]\\d\\d?(?::?\\d\\d)?");
+
+	/**
+	 * Checks whether a string is a valid representaion of a time zone in the absolute format.
+	 * <p> Checking is performed according to the AbsTimeZone data type.
+	 * @param	timeZoneStr The string to check.
+	 * @return	<tt>true</tt> if <tt>timeZoneStr</tt> is valid, <tt>false</tt> otherwise.
+	 */
+	public static boolean checkAbsTimeZone(String timeZoneStr) {
+		return ABS_TIME_ZONE_PATTERN.matcher(timeZoneStr).matches();
 	}
 
-	public static boolean checkName(String login) {
-		if (login.isEmpty()) {
-			return false;
-		}
-		if (!isLetter(login.charAt(0))) {
-			return false;
-		}
-		for (int i = 1; i < login.length(); ++i) {
-			if (!(isLetter(login.charAt(i)) || isDigit(login.charAt(i)))) {
-				return false;
-			}
-		}
-		return true;
+	private static final Pattern DATE_TIME_PATTERN =
+		Pattern.compile("\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}");
+
+	/**
+	 * Checks whether a string is a valid representaion of a time zone in the absolute format.
+	 * <p> Checking is performed according to the AbsTimeZone data type.
+	 * @param	timeZoneStr The string to check.
+	 * @return	<tt>true</tt> if <tt>timeZoneStr</tt> is valid, <tt>false</tt> otherwise.
+	 */
+	public static boolean checkLocalDateTime(String localDateTimeStr) {
+		return DATE_TIME_PATTERN.matcher(localDateTimeStr).matches();
 	}
 
-/*	public static boolean checkTimeZone(String timeZone) {
-		return true; // FIXME
-	}
-*/
-	public static boolean checkActive(String active) {
-		return active.equals("true") || active.equals("false");
+	/**
+	 * Checks whether a string is a valid representaion of a user's status.
+	 * <p> Checking is performed according to the Boolean data type.
+	 * @param	activeStr The string to check.
+	 * @return	<tt>true</tt> if <tt>activeStr</tt> is valid, <tt>false</tt> otherwise.
+	 */
+	public static boolean checkActive(String activeStr) {
+		return activeStr.equals("true") || activeStr.equals("false");
 	}
 }
